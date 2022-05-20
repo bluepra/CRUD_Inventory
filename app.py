e68afed376a6f8bf915d3590773fe2d15e700d01
bluepra <parora9@wisc.edu>
2022-05-19 23:05:54 -0500

    Started working on edit

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 # Models for the Database
 class Item(db.Model):
 	pid = db.Column(db.Integer, primary_key = True) # Product/Item ID - primary key
@@ -18,6 +19,7 @@ class Item(db.Model):
 	def __repr__(self):
 		return f"Item('{self.pid}', '{self.name}')"
 
+
 class Warehouse(db.Model):
 	wid = db.Column(db.Integer, primary_key = True) # Warehouse ID - primary key
 	name = db.Column(db.String(100), nullable = False) # Warehouse Name
@@ -26,6 +28,7 @@ class Warehouse(db.Model):
 
 	def __repr__(self):
 		return f"Warehouse('{self.wid}', '{self.name}')"
+
 
 # Default (Inventory) page
 @app.route("/", methods = ["POST", "GET"])
@@ -85,7 +88,7 @@ def warehouses_page():
 		return redirect("/warehouses")
 
 
-@app.route("/delete_item/<int:pid>", methods = ["POST", "GET"])
+@app.route("/delete_item/<int:pid>")
 def delete_item(pid):
 	item_to_remove = db.session.query(Item).get_or_404(pid)
 
@@ -98,7 +101,7 @@ def delete_item(pid):
 	return redirect("/")
 
 
-@app.route("/delete_warehouse/<int:wid>", methods = ["POST", "GET"])
+@app.route("/delete_warehouse/<int:wid>")
 def delete_warehouse(wid):
 	warehouse_to_remove = db.session.query(Warehouse).get_or_404(wid)
 
@@ -109,7 +112,11 @@ def delete_warehouse(wid):
 		return f"Failed to delete warehouse {wid}"
 	
 	return redirect("/warehouses")
-	
+
+
+@app.route("/edit_item/<int:pid>", methods = ["POST", "GET"])
+def edit_item(pid):
+	return render_template("edit_item.html", pid = pid)
 
 if __name__ == "__main__":  
 	app.run(debug = True)
